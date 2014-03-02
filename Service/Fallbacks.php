c29aaccc26f52f4d840f27a498ae2ab41a1858d9
Tijs Verkoyen <tijs@verkoyen.eu>
2014-03-02 01:25:38 +0100

    Introduced the Fallback object
    The purpose of this object is to be a "container" that handles retrieving of
    some fallback values. For instance the site title.
    
    The Fallback object will be registered as a service, so it can be used by Twig
    and other components.

---
+++ Service/Fallbacks.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace SumoCoders\FrameworkCoreBundle\Service;
+
+
+/**
+ * Class Fallbacks
+ *
+ * @package SumoCoders\FrameworkCoreBundle\Service
+ */
+class Fallbacks
+{
+    /**
+     * @var array
+     */
+    private $fallbacks = array();
+
+    /**
+     * @param array $parameters
+     */
+    public function __construct(array $parameters = null)
+    {
+        $this->populateFallbacks($parameters);
+    }
+
+    /**
+     * Add a value into the array with a given name
+     *
+     * @param string      $name
+     * @param mixed       $value
+     * @param null|string $prefix
+     */
+    protected function addFallback($name, $value, $prefix = null)
+    {
+        $key = $name;
+        if ($prefix) {
+            $key = $prefix . '.' . $name;
+        }
+        $this->fallbacks[$key] = $value;
+    }
+
+    /**
+     * Populate the array with data, can be called recursive
+     *
+     * @param mixed|array $data
+     * @param null|string $prefix
+     */
+    private function populateFallbacks($data, $prefix = null)
+    {
+        // anything to process?
+        if (empty($data)) {
+            return;
+        }
+
+        // loop all data
+        foreach ($data as $name => $value) {
+            // if it is an array we want to process some more
+            if (is_array($value)) {
+                // if there is a numeric key inside the keys we will treat
+                // the array as an key-value-array, otherwise we will assume
+                // the array was intended as an array
+                $anyNonNumericKeys = false;
+                $keys = array_keys($value);
+                foreach ($keys as $key) {
+                    if (!is_numeric($key)) {
+                        $anyNonNumericKeys = true;
+                        break;
+                    }
+                }
+
+                if ($anyNonNumericKeys) {
+                    $this->populateFallbacks($value, $name);
+                } else {
+                    $this->addFallback($name, $value, $prefix);
+                }
+            } else {
+                $this->addFallback($name, $value, $prefix);
+            }
+        }
+    }
+
+    /**
+     * Get a fallback
+     *
+     * @param string $key
+     * @return null|mixed
+     */
+    public function get($key)
+    {
+        if (isset($this->fallbacks[$key])) {
+            return $this->fallbacks[$key];
+        }
+
+        return null;
+    }
+}
---
+++ Tests/Service/FallbacksTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SumoCoders\FrameworkCoreBundle\Tests\Service;
+
+require_once __DIR__ . '/../../Service/Fallbacks.php';
+
+use SumoCoders\FrameworkCoreBundle\Service\Fallbacks;
+
+class FallbacksTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Fallbacks
+     */
+    private $fallbacks;
+
+    /**
+     * @var array
+     */
+    private $defaultData = array(
+        'foo' => 'bar',
+        'name' => array(
+            'first' => 'John',
+            'last' => 'Doe',
+        ),
+        'servers' => array(
+            'web01',
+            'web02',
+        ),
+        'a' => array(
+            'very' => array(
+                'deep' => array(
+                    'array' => array(
+                        'x' => 0,
+                        'y' => 0,
+                    )
+                )
+            ),
+        ),
+    );
+
+    /**
+     * Tests Fallbacks->get()
+     */
+    public function testGet()
+    {
+        $this->fallbacks = new Fallbacks($this->defaultData);
+
+        $this->assertEquals($this->defaultData['foo'], $this->fallbacks->get('foo'));
+        $this->assertEquals($this->defaultData['name']['first'], $this->fallbacks->get('name.first'));
+        $this->assertNull($this->fallbacks->get('name')); // we can't grab a key-value-array
+        $this->assertEquals($this->defaultData['servers'], $this->fallbacks->get('servers'));
+        $this->assertEquals(
+            $this->defaultData['a']['very']['deep']['array']['x'],
+            $this->fallbacks->get('a.very.deep.array.x')
+        );
+    }
+}

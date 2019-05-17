66707dbab864380cc10ea2e3e8902793457e750f
Bruno Vitorino <bruno@sumocoders.be>
2019-05-17 10:48:55 +0200

    Improve code style

--- Form/Extension/DateTypeExtension.php
+++ Form/Extension/DateTypeExtension.php
@@ -62,8 +62,10 @@ final class DateTypeExtension extends AbstractTypeExtension
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $view->vars['maximum_date'] = $options['maximum_date'] ? IntlDateFormatter::formatObject($options['maximum_date'], $options['format']) : null;
-        $view->vars['minimum_date'] = $options['minimum_date'] ? IntlDateFormatter::formatObject($options['minimum_date'], $options['format']) : null;
+        $view->vars['maximum_date'] = $options['maximum_date'] ?
+            IntlDateFormatter::formatObject($options['maximum_date'], $options['format']) : null;
+        $view->vars['minimum_date'] = $options['minimum_date'] ?
+            IntlDateFormatter::formatObject($options['minimum_date'], $options['format']) : null;
         $view->vars['format'] = $options['format'];
         $view->vars['divider'] = (strpos($options['format'], '-') !== false) ? '-' : '/';
         $view->vars['datepicker'] = $options['datepicker'] ?? false;

856ced031fb3149186fc09d8707691dd964adbb1
Bjorn Van Acker <bjorn@sumocoders.be>
2020-04-02 11:16:52 +0200

    Don't try to load twig

--- DependencyInjection/SumoCodersFrameworkCoreExtension.php
+++ DependencyInjection/SumoCodersFrameworkCoreExtension.php
@@ -21,7 +21,6 @@ class SumoCodersFrameworkCoreExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
-        $loader->load('twig.yml');
         $loader->load('parameters.yml');
     }
 }

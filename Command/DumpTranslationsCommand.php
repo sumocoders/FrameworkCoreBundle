549a94da6d00ea440d19cad54d18a03b6fc71e0d
Sander De la Marche <sander@sumocoders.be>
2021-05-04 15:55:39 +0200

    Wrap the translation update command in something easier to use
    THis way, we can set the default to yaml and access our locales from the services.yml config

---
+++ Command/DumpTranslationsCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class DumpTranslationsCommand extends Command
+{
+    private ParameterBagInterface $parameters;
+
+    public function __construct(ParameterBagInterface $parameters)
+    {
+        parent::__construct();
+        $this->setName('sumo:dump:translations');
+        $this->parameters = $parameters;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $locale = $this->parameters->get('locale');
+
+        $command = $this->getApplication()->find('translation:update');
+
+        $arguments = [
+            'locale' => $locale,
+            '--force' => true,
+            '--output-format' => 'yaml',
+            '--clean' => true,
+        ];
+
+        $translationInput = new ArrayInput($arguments);
+
+        return $command->run($translationInput, $output);
+    }
+}

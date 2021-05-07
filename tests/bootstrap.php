195ace2bbb251cb0dd48d5be43ad1f6044c2878c
Sander De la Marche <sander@sumocoders.be>
2021-05-07 11:20:01 +0200

    Make the tests work in their new location

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
-call_user_func(function () {
-    if (! is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
-        throw new \LogicException('Could not find vendor/autoload.php. Did you forget to run "composer install --dev"?');
-    }
+$file = __DIR__ . '/../vendor/autoload.php';
 
-    require $autoloadFile;
-});
+if (!file_exists($file)) {
+    throw new RuntimeException('Install dependencies using Composer to run the test suite.');
+}
+
+$autoload = require $file;

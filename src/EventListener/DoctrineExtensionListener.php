bc67083b17e921a6bbcf72ed5d8adcf104a06053
Sander De la Marche <sander@sumocoders.be>
2021-09-08 10:17:42 +0200

    Use property type hinting for the Doctrine listener

--- src/EventListener/DoctrineExtensionListener.php
+++ src/EventListener/DoctrineExtensionListener.php
@@ -4,35 +4,27 @@ namespace SumoCoders\FrameworkCoreBundle\EventListener;
 
 use Gedmo\Blameable\BlameableListener;
 use Gedmo\Loggable\LoggableListener;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class DoctrineExtensionListener
 {
-    /** @var TokenStorageInterface */
-    private $tokenStorage;
-
-    /** @var AuthorizationCheckerInterface */
-    private $authorizationChecker;
-
-    /** @var LoggableListener */
-    private $loggableListener;
-
-    /** @var BlameableListener */
-    private $blameableListener;
+    private TokenStorageInterface $tokenStorage;
+    private AuthorizationCheckerInterface $authorizationChecker;
+    private LoggableListener $loggableListener;
+    private BlameableListener $blamableListener;
 
     public function __construct(
         TokenStorageInterface $tokenStorage,
         AuthorizationCheckerInterface $authorizationChecker,
         LoggableListener $loggableListener,
-        BlameableListener $blameableListener
+        BlameableListener $blamableListener
     ) {
         $this->tokenStorage = $tokenStorage;
         $this->authorizationChecker = $authorizationChecker;
         $this->loggableListener = $loggableListener;
-        $this->blameableListener = $blameableListener;
+        $this->blamableListener = $blamableListener;
     }
 
     public function onKernelRequest(RequestEvent $event)
@@ -41,7 +33,7 @@ class DoctrineExtensionListener
             $user = $this->tokenStorage->getToken()->getUser();
 
             $this->loggableListener->setUsername($user);
-            $this->blameableListener->setUserValue($user);
+            $this->blamableListener->setUserValue($user);
         }
     }
 }

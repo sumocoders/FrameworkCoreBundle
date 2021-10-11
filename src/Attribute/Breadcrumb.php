8b583e8ae798d69f5038551103cb72dafa03ec55
Sander De la Marche <sander@sumocoders.be>
2021-10-11 16:37:15 +0200

    Add the new Breadcrumb attribute

---
+++ src/Attribute/Breadcrumb.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace SumoCoders\FrameworkCoreBundle\Attribute;
+
+use Attribute;
+use SumoCoders\FrameworkCoreBundle\ValueObject\Route;
+
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD)]
+final class Breadcrumb
+{
+    private string $title;
+    private ?Route $route;
+    private ?Route $parent;
+
+    public function __construct(
+        string $title,
+        ?array $route = null,
+        ?array $parent = null
+    ) {
+        $this->title = $title;
+
+        if ($route !== null) {
+            $this->route = new Route($route['name'], \array_key_exists('parameters', $route) ? $route['parameters'] : null);
+        } else {
+            $this->route = $route;
+        }
+
+        if ($parent !== null) {
+            $this->parent = new Route($parent['name'], \array_key_exists('parameters', $parent) ? $parent['parameters'] : null);
+        } else {
+            $this->parent = $parent;
+        }
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function getRoute(): ?Route
+    {
+        return $this->route;
+    }
+
+    public function setRoute(Route $route): void
+    {
+        $this->route = $route;
+    }
+
+    public function getParent(): ?Route
+    {
+        return $this->parent;
+    }
+
+    public function hasRoute(): bool
+    {
+        return $this->route !== null;
+    }
+
+    public function hasParent(): bool
+    {
+        return $this->parent !== null;
+    }
+
+}
---
+++ src/ValueObject/Route.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SumoCoders\FrameworkCoreBundle\ValueObject;
+
+class Route
+{
+    private string $name;
+    private ?array $parameters;
+
+    public function __construct(string $name, ?array $parameters)
+    {
+        $this->name = $name;
+        $this->parameters = $parameters;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getParameters(): ?array
+    {
+        return $this->parameters;
+    }
+}

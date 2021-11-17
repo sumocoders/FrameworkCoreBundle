bd97b433356b3008c05d0f43730246af12fdcd1a
Sander De la Marche <sander@sumocoders.be>
2021-11-17 15:50:38 +0100

    Add missing return type

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('sumo_coders_framework_core');
 

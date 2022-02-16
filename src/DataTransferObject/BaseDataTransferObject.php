2c8607384ffe23517f92c2a9a5d4339ba05d93e3
Sander De la Marche <sander@sumocoders.be>
2022-02-16 22:20:51 +0100

    Improve the base DTO class
    This iteration will handle recursion, so nested DTOs will
    not be a problem anymore.
    
    It also works with the constructor instead of the static
     `from` method call. The only downside to this approach is
    that the source objetc that is passed to the DTO isn't
    properly type-hinted when you fetch it using the getSource
    method. You need to provide an inline @var docblock before
    your IDE will autocomplete it and PHPStan will accept it.

--- src/DataTransferObject/BaseDataTransferObject.php
+++ src/DataTransferObject/BaseDataTransferObject.php
@@ -1,36 +1,56 @@
 <?php
 
-namespace SumoCoders\FrameworkCoreBundle\DataTransferObject;
+namespace App\DataTransferObject;
 
-use SumoCoders\FrameworkCoreBundle\Exception\DataTransferExceptionException;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 
 class BaseDataTransferObject
 {
-    public static function from(object $entity): static
+    protected object $source;
+
+    final public function __construct(?object $source = null)
     {
+        if ($source === null) {
+            return;
+        }
+
+        $this->source = $source;
+
         $propertyAccessor = PropertyAccess::createPropertyAccessor();
         $propertyInfo = new PropertyInfoExtractor(
-            [new ReflectionExtractor()]
+            [new ReflectionExtractor()],
+            [new ReflectionExtractor()],
+            [],
+            [new ReflectionExtractor()],
         );
 
         // Read all public properties of the child DTO that extends this class
         $properties = $propertyInfo->getProperties(static::class);
 
-        if (empty($properties)) {
-            throw new DataTransferExceptionException(sprintf('Found no public properties in $%1s.', static::class));
-        }
-
-        // Create a new instance of our child DTO
-        $dataTransferObject = new static();
-
-        // For each public property, try to look for a getter in the entity
         foreach ($properties as $property) {
-            $dataTransferObject->{$property} = $propertyAccessor->getValue($entity, $property);
-        }
+            // Only continue if the property is writable (a.k.a. public)
+            if ($propertyInfo->isWritable(static::class, $property)) {
+                // Get the value from the passed source object
+                $sourceValue = $propertyAccessor->getValue($source, $property);
 
-        return $dataTransferObject;
+                // Get the property type & class name
+                $type = $propertyInfo->getTypes(static::class, $property)[0];
+                $className = $type->getClassName();
+
+                // If the property is a class that extends this class, handle it recursively
+                if ($className !== null && is_subclass_of($className, self::class)) {
+                    $this->{$property} = new $className($sourceValue);
+                } else {
+                    $this->{$property} = $sourceValue;
+                }
+            }
+        }
+    }
+
+    public function getSource(): object
+    {
+        return $this->source;
     }
 }

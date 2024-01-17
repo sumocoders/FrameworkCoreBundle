0a522e6e7490daa9b48cf9f9eb04620037905aef
Bjorn Van Acker <bjorn@sumocoders.be>
2024-01-17 16:05:12 +0100

    Add validation

--- src/Form/Extension/CollectionTypeExtension.php
+++ src/Form/Extension/CollectionTypeExtension.php
@@ -3,10 +3,14 @@
 namespace SumoCoders\FrameworkCoreBundle\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
 
 final class CollectionTypeExtension extends AbstractTypeExtension
 {
@@ -26,8 +30,48 @@ final class CollectionTypeExtension extends AbstractTypeExtension
                 'add_button_label' => 'forms.buttons.addItem',
                 'minimum_required_items' => 0,
                 'maximum_required_items' => null,
+                'error_bubbling' => false,
             ]
         );
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        parent::buildForm($builder, $options);
+
+        if ($options['minimum_required_items'] < 0) {
+            throw new \InvalidArgumentException('minimum_required_items cannot be lower than 0');
+        }
+
+        if ($options['maximum_required_items'] !== null && $options['maximum_required_items'] < $options['minimum_required_items']) {
+            throw new \InvalidArgumentException('maximum_required_items cannot be lower than minimum_required_items');
+        }
+
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) {
+            $form = $event->getForm();
+            $min = $form->getConfig()->getOption('minimum_required_items');
+            $max = $form->getConfig()->getOption('maximum_required_items');
+
+            if ($form->count() < $min) {
+                $error = new FormError(
+                    message: 'You must add at least '.$min.' items',
+                    messageTemplate: 'You must add at least %count% items',
+                    messageParameters: ['%count%' => $min]
+                );
+                $error->setOrigin($form);
+                $form->addError($error);
+            }
+
+            if ($max !== null && $form->count() > $max) {
+                $error = new FormError(
+                    message: 'You can add a maximum of '.$max.' items',
+                    messageTemplate: 'You can add a maximum of %count% items',
+                    messageParameters: ['%count%' => $max]
+                );
+                $error->setOrigin($form);
+                $form->addError($error);
+            }
+        });
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options): void

321f8c2e3646cf1cbffbc1cce2dbadc64de28028
Bjorn Van Acker <bjorn@sumocoders.be>
2024-03-25 13:24:19 +0100

    Add password strength service

---
+++ src/Security/PasswordStrengthService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace SumoCoders\FrameworkCoreBundle\Security;
+
+class PasswordStrengthService
+{
+    public const STRENGTH_VERY_WEAK = 0;
+    public const STRENGTH_WEAK = 1;
+    public const STRENGTH_MEDIUM = 2;
+    public const STRENGTH_STRONG = 3;
+    public const STRENGTH_VERY_STRONG = 4;
+
+    /**
+     * Returns the estimated strength of a password.
+     *
+     * The higher the value, the stronger the password.
+     *
+     * @return PasswordStrength::STRENGTH_*
+     */
+    public function estimateStrength(
+        string $password
+    ): int  {
+        if (!$length = \strlen($password)) {
+            return self::STRENGTH_VERY_WEAK;
+        }
+        $password = count_chars($password, 1);
+        $chars = \count($password);
+
+        $control = $digit = $upper = $lower = $symbol = $other = 0;
+        foreach ($password as $chr => $count) {
+            match (true) {
+                $chr < 32 || 127 === $chr => $control = 33,
+                48 <= $chr && $chr <= 57 => $digit = 10,
+                65 <= $chr && $chr <= 90 => $upper = 26,
+                97 <= $chr && $chr <= 122 => $lower = 26,
+                128 <= $chr => $other = 128,
+                default => $symbol = 33,
+            };
+        }
+
+        $pool = $lower + $upper + $digit + $symbol + $control + $other;
+        $entropy = $chars * log($pool, 2) + ($length - $chars) * log($chars, 2);
+
+        return match (true) {
+            $entropy >= 120 => self::STRENGTH_VERY_STRONG,
+            $entropy >= 100 => self::STRENGTH_STRONG,
+            $entropy >= 80 => self::STRENGTH_MEDIUM,
+            $entropy >= 60 => self::STRENGTH_WEAK,
+            default => self::STRENGTH_VERY_WEAK,
+        };
+    }
+}

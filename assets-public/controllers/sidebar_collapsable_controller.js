d05ecf050a0ad468352bb9ffbf88569a53c65749
Jonas De Keukelaere <jonas@sumocoders.be>
2024-11-08 08:36:19 +0000

    Use cookie for sidebar toggle status
    So the sidebar does start open and get closed by javascript given a sort of "flickering effect".

--- assets-public/controllers/sidebar_collapsable_controller.js
+++ assets-public/controllers/sidebar_collapsable_controller.js
@@ -1,23 +1,13 @@
 import { Controller } from '@hotwired/stimulus';
+import { setCookie } from 'sumocoders/cookie'
 
 export default class extends Controller {
   static targets = ['toggleable']
 
-  connect () {
-    if (window.innerWidth > 576) {
-      // read local storage
-      if (localStorage.getItem('sidebar_is_open') === 'false') {
-        this.toggleableTarget.classList.add('sidebar-collapsed')
-      } else {
-        this.toggleableTarget.classList.remove('sidebar-collapsed')
-      }
-    }
-  }
-
   toggle () {
     this.toggleableTarget.classList.toggle('sidebar-collapsed');
 
-    // set local storage
-    localStorage.setItem('sidebar_is_open', this.toggleableTarget.classList.contains('sidebar-collapsed') ? 'false' : 'true')
+    // set cookie
+    setCookie('sidebar_is_open', !this.toggleableTarget.classList.contains('sidebar-collapsed'))
   }
 }

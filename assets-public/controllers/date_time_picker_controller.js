faad383f2074726436505bdd78b168dfc1ad7262
Jonas De Keukelaere <jonas@sumocoders.be>
2024-11-20 08:56:40 +0000

    Allow typing in date time fields

--- assets-public/controllers/date_time_picker_controller.js
+++ assets-public/controllers/date_time_picker_controller.js
@@ -37,7 +37,8 @@ export default class extends Controller {
     this.element._flatpickr = flatpickr(this.element, {
       locale,
       enableTime: this.enableTimeValue,
-      noCalendar: !this.showCalendarValue
+      noCalendar: !this.showCalendarValue,
+      allowInput: true
     })
   }
 }

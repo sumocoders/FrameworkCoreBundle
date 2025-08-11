5469c18a95cb45515258e2ed516cff278d7bc43b
Abraham <abraham@sumocoders.be>
2025-08-11 13:12:10 +0200

    Throw an exception when file is not readable

--- src/Twig/ContentExtension.php
+++ src/Twig/ContentExtension.php
@@ -3,19 +3,21 @@
 namespace SumoCoders\FrameworkCoreBundle\Twig;
 
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Filesystem\Filesystem;
 use Twig\Attribute\AsTwigFunction;
 
 readonly class ContentExtension
 {
     public function __construct(
         #[Autowire('%kernel.project_dir%/public')]
-        private string $publicFolder
+        private string $publicFolder,
+        private Filesystem $filesystem
     ) {
     }
 
     #[AsTwigFunction('content', isSafe: ['html'])]
     public function getContent(string $path): string
     {
-        return file_get_contents($this->publicFolder . $path);
+        return $this->filesystem->readFile($this->publicFolder . $path);
     }
 }
